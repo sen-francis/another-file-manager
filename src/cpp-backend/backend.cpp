8d32c5f9408887d79594bb0ee3a41ee0f5cd52c1
sen-francis <s3nfrancis@gmail.com>
2021-10-11 01:57:31 -0400

    Added comments to c++ file.

--- src/cpp-backend/backend.cpp
+++ src/cpp-backend/backend.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <string>
 
+// Returns abbreviated string of month given index.
 std::string backend::getMonthIndex(std::string month)
 {
     std::unordered_map<std::string, std::string> months
@@ -25,9 +26,7 @@ std::string backend::getMonthIndex(std::string month)
         { "Nov", "11" },
         { "Dec", "12" }
     };
-
     const auto iter = months.find(month);
-
     if( iter != months.cend() )
         return iter->second;
     return "ERROR";
@@ -40,29 +39,24 @@ bool backend::dateRangeComparison(std::string date, std::pair<std::string, std::
   std::stringstream dateStream(date);
   std::stringstream fromDateStream(dateRange.first);
   std::stringstream toDateStream(dateRange.second);
-
   // Get dates in terms of seconds so easy to compare
   std::tm dateTime = {0};
   dateStream >> std::get_time( &dateTime, "%m/%d/%Y" );
   std::time_t dateSeconds = std::mktime( & dateTime );
-
   std::tm fromDateTime = {0};
   fromDateStream >> std::get_time( &fromDateTime, "%m/%d/%Y" );
   std::time_t fromDateSeconds = std::mktime( & fromDateTime );
-
   std::tm toDateTime = {0};
   toDateStream >> std::get_time( &toDateTime, "%m/%d/%Y" );
   std::time_t toDateSeconds = std::mktime( & toDateTime );
-
   // If date is in range return true, else return false
   if(dateSeconds >= fromDateSeconds && dateSeconds <= toDateSeconds){
     return true;
   }
-
   return false;
 }
 
-// Converts date to MM/DD/YYYY Format
+// Returns a string in MM/DD/YYYY Format
 std::string backend::dateConverter(std::string date){
   // date is originally in this example format: "Sat Sep 19 18:57:40 2020"
   // after this modification, date should be: "Sep 19 2020"
@@ -72,8 +66,10 @@ std::string backend::dateConverter(std::string date){
   return date;
 }
 
+// Returns a vector of file names mactching search criteria.
 std::vector<std::string> backend::search(std::string folderPath, std::pair<std::string, std::string> accessedDates, std::pair<std::string, std::string> modifiedDates){
   std::vector<std::string> files;
+  // Iterates through all files in directory.
   for (const auto & entry : std::filesystem::directory_iterator(folderPath)){
     std::string file = entry.path().u8string();
     struct stat t_stat;
@@ -86,20 +82,20 @@ std::vector<std::string> backend::search(std::string folderPath, std::pair<std::
         continue;
       }
     }
-
     if(modifiedDates.first != "NULL" && modifiedDates.second != "NULL"){
       if(!dateRangeComparison(modifiedDate, modifiedDates)){
         // If date is not in between given date range, don't add file
         continue;
       }
     }
-
     // Only add file if passes criteria
     files.push_back(file);
   }
   return files;
 }
 
+// Moves input files into folder and deletes folder.
+// Returns deleted folder path.
 std::string backend::deleteFiles(std::vector<std::string> fileNames){
   //get current date for folder namespace
   std::time_t t = std::time(0);
@@ -124,6 +120,7 @@ std::string backend::deleteFiles(std::vector<std::string> fileNames){
 // Function serves as link between JS and C++ so that objects can be converted between languages
 Napi::Array backend::searchWrapped(const Napi::CallbackInfo& info){
   Napi::Env env = info.Env();
+  // Check that correct number of arguments provided.
   if (info.Length() < 5) {
       Napi::TypeError::New(env, "Incorrect number of arguments.").ThrowAsJavaScriptException();
   }
@@ -133,10 +130,8 @@ Napi::Array backend::searchWrapped(const Napi::CallbackInfo& info){
   std::string accessedToDate = info[2].ToString().Utf8Value();
   std::string modifiedFromDate = info[3].ToString().Utf8Value();
   std::string modifiedToDate = info[4].ToString().Utf8Value();
-
   // Call search function
   std::vector<std::string> results = backend::search(folderPath, std::make_pair(accessedFromDate, accessedToDate), std::make_pair(modifiedFromDate, modifiedToDate));
-
   // Iterate through vector to create Napi::Array based on results
   Napi::Array napiResults = Napi::Array::New(env, results.size());
   for (int i = 0; i < results.size(); i++){
@@ -145,20 +140,26 @@ Napi::Array backend::searchWrapped(const Napi::CallbackInfo& info){
   return napiResults;
 }
 
+// Function serves as link between JS and C++ so that objects can be converted between languages
 Napi::String backend::deleteFilesWrapped(const Napi::CallbackInfo& info){
   Napi::Env env = info.Env();
+  // Check that correct number of arguments provided.
   if (info.Length() < 1) {
       Napi::TypeError::New(env, "Incorrect number of arguments.").ThrowAsJavaScriptException();
   }
+  // Parsing input array.
   Napi::Array fileNapiArray = info[0].As<Napi::Array>();
   std::vector<std::string> filesVector;
   for(int i = 0; i < fileNapiArray.Length(); i++){
     filesVector.push_back(static_cast<Napi::Value>(fileNapiArray[i]).ToString().Utf8Value());
   }
+  // Call deleteFiles function.
   std::string folderName = backend::deleteFiles(filesVector);
+  // Return deleted folder path.
   return Napi::String::New(env,folderName);
 }
 
+// Function registered keywords so functions can be called in JS.
 Napi::Object backend::Init(Napi::Env env, Napi::Object exports)
 {
   exports.Set("search", Napi::Function::New(env, backend::searchWrapped));
